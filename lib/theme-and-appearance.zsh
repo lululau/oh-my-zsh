c24837077bf84ff265dd21dd4c73e0827b0b7dd6
Liu Xiang <liuxiang921@gmail.com>
2017-09-12 23:09:46 +0800

    Comment out postiche config

--- lib/theme-and-appearance.zsh
+++ lib/theme-and-appearance.zsh
@@ -2,7 +2,7 @@
 autoload -U colors && colors
 
 # Enable ls colors
-export LSCOLORS="Gxfxcxdxbxegedabagacad"
+# export LSCOLORS="Gxfxcxdxbxegedabagacad"
 
 # TODO organise this chaotic logic
 
@@ -25,7 +25,7 @@ if [[ "$DISABLE_LS_COLORS" != "true" ]]; then
 
     # only use coreutils ls if there is a dircolors customization present ($LS_COLORS or .dircolors file)
     # otherwise, gls will use the default color scheme which is ugly af
-    [[ -n "$LS_COLORS" || -f "$HOME/.dircolors" ]] && gls --color -d . &>/dev/null && alias ls='gls --color=tty'
+    # [[ -n "$LS_COLORS" || -f "$HOME/.dircolors" ]] && gls --color -d . &>/dev/null && alias ls='gls --color=tty'
   else
     # For GNU ls, we use the default ls color theme. They can later be overwritten by themes.
     if [[ -z "$LS_COLORS" ]]; then
